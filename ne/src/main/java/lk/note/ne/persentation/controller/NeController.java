5dfbbedbc1a701a855c78115b9a4aa17fb5e073d
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2026-03-25 12:48:54 +0530

    create api server side

--- ne/src/main/java/lk/note/ne/persentation/controller/NeController.java
+++ ne/src/main/java/lk/note/ne/persentation/controller/NeController.java
@@ -1,14 +1,40 @@
 package lk.note.ne.persentation.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.note.ne.persentation.dto.NeRequest;
+import lk.note.ne.persentation.dto.NeResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/ne")
 public class NeController {
 
 
-    
+    @PostMapping
+    public ResponseEntity<NeResponse> saveNote(@RequestBody NeRequest neRequest) {
+        return new ResponseEntity<>(new NeResponse(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNote(@PathVariable UUID id) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NeResponse> updateNote(@RequestBody NeRequest neRequest, @PathVariable UUID id) {
+        return ResponseEntity.ok(new NeResponse());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<NeResponse>> getAllNotes() {
+        return ResponseEntity.ok(new ArrayList<>());
+    }
+
 
 }
