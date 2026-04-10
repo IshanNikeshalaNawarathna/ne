391daf85b3096a4b92db2fa05bcd0a7faa803960
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2026-04-11 02:16:13 +0530

    update api.tsx and kubernetes files

--- notes-app/src/services/api.ts
+++ notes-app/src/services/api.ts
@@ -41,7 +41,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "",
-  baseURL: 'http://127.0.0.1:10198',
+  baseURL: 'http://notes.local/api/v1/ne',
   headers: {
     "Content-Type": "application/json",
   },
