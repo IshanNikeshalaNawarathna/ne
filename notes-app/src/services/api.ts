e062f0c71cb650c180dc272cba7709c398e1cfe0
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2026-04-13 23:54:04 +0530

    update api files

--- notes-app/src/services/api.ts
+++ notes-app/src/services/api.ts
@@ -40,7 +40,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || '/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || '',
   headers: {
     "Content-Type": "application/json",
   },
